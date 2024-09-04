64d5ba2df78ebb9e6af295a6b704bbe213d3755b
Guilherme <pepe.ks.rx.1@gmail.com>
2024-09-03 22:20:27 -0300

    Ajustes na Servlet para não passar o usuário ao errar o login

--- src/main/java/servlet/LoginUsuarioServlet.java
+++ src/main/java/servlet/LoginUsuarioServlet.java
@@ -30,7 +30,7 @@ public class LoginUsuarioServlet extends HttpServlet {
                 resp.sendRedirect("Principal/principal.jsp");
             }else {
                 req.setAttribute("errorMessage", "Login inválido. Verifique o login e a senha!");
-                req.getRequestDispatcher("login.jsp").forward(req, resp);
+                req.getRequestDispatcher("Login/login.jsp").forward(req, resp);
             }
 
         }catch (Exception err){
